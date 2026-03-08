83b0e81a615b274b161a309555d6dff050bf51f1
Ethan94FL <fabioleo290@gmail.com>
2026-03-08 21:46:05 +0100

    doc: documentazione

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,4 @@
-const version = "1.1.1";
+const version = "1.1.1"; // MAJOR, MINOR, PATCH
 const input = document.querySelector("input");
 const searchBoxContainer = document.body.querySelector(".sections-buttons-container");
 const table = document.body.querySelector("table");
